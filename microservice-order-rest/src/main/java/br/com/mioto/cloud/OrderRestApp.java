0f59c0211ecf7b264ab0adaf4073c7bc5986a6c8
Eduardo Mioto <eduardo.mioto@philips.com>
2019-10-19 12:05:23 -0300

    update
    update

--- microservice-order-rest/src/main/java/br/com/mioto/cloud/OrderRestApp.java
+++ microservice-order-rest/src/main/java/br/com/mioto/cloud/OrderRestApp.java
@@ -1,18 +1,16 @@
 package br.com.mioto.cloud;
 
-import br.com.mioto.cloud.controllers.ConsulController;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mioto.cloud.controllers.ConsulController;
 
 @Configuration
 @EnableAutoConfiguration
@@ -20,14 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableConfigurationProperties
 @EnableFeignClients
-@Slf4j
 @SpringBootApplication
 public class OrderRestApp {
-
-    @Bean
-    public AlwaysSampler defaultSampler() {
-        return new AlwaysSampler();
-    }
 
     /**
      * The main method.
@@ -35,7 +27,7 @@ public class OrderRestApp {
      * @param args the arguments
      */
     public static void main( String[] args ) {
-        ConfigurableApplicationContext context = SpringApplication.run(OrderRestApp.class, args);
+        final ConfigurableApplicationContext context = SpringApplication.run(OrderRestApp.class, args);
         //After Start process, create or update dependencies entry on KV Store from Service Discovery
         context.getBean(ConsulController.class).kvPut();
     }
